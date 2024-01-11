3b70b371d889021aa8b842c8aac6cc27282199cc
Glitch (michelepeixoto) <none>
2024-01-11 06:25:58 +0000

    🧸🔣 Updated with Glitch

--- script.js
+++ script.js
@@ -8,6 +8,25 @@ function hideEmail(){
 
 /* NOT IN USE ----------------------------------
 
+// Window width display
+window.addEventListener('load', function(event){
+  var width_tool = document.createElement("DIV");
+  width_tool.style.width = "100%";
+  width_tool.style.position = "fixed";
+  width_tool.style.top = "0";
+  width_tool.id = "window-size";
+  var text = document.createTextNode("Window width: " + window.innerWidth + ", height: " + window.innerHeight);
+  width_tool.appendChild(text);
+  document.body.appendChild(width_tool);
+});
+
+window.addEventListener('resize', function(event){
+  var div = document.getElementById("window-size");
+  div.innerHTML = "Window width: " + window.innerWidth + ", height: " + window.innerHeight;
+});
+// Window width display
+
+
 console.log("loaded"); 
 
 var current = "", item = ""; 
